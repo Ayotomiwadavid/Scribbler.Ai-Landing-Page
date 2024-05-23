eb6fea8bbb505765c0672544d0382cc89b3ca893
Techthoth <ayotomiwadavid@proton.me>
2024-05-23 09:25:48 -0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Button } from "flowbite-react";
+
+const App = () => {
+  return (
+    <div className="w-full h-lvh flex items-center justify-center bg-slate-400">
+      <Button>Click me</Button>
+    </div>
+  );
+};
+
+export default App;
